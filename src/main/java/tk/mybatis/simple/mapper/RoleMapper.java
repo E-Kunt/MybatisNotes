9a6945604dc93fd022bbe3172d7844bf7bda457a
E-Kunt <ethanwyk@sina.com>
2018-05-16 01:06:30 +0800

    anotaion about @Results & @Result & @ResultMap.

--- src/main/java/tk/mybatis/simple/mapper/RoleMapper.java
+++ src/main/java/tk/mybatis/simple/mapper/RoleMapper.java
@@ -1,5 +1,11 @@
 package tk.mybatis.simple.mapper;
 
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import tk.mybatis.simple.model.SysRole;
@@ -12,7 +18,7 @@ public interface RoleMapper {
 	 * @return
 	 */
 	/*
-	 @Select({"select id, role_name, enabled, create_by, create_time ",
+	 @Select({"select id, role_name roleName, enabled, create_by createBy, create_time createTime ",
 			 "from sys_role ",
 			 "where id = #{id}"})
 	 */
@@ -21,5 +27,22 @@ public interface RoleMapper {
 			 "where id = #{id}"})
 	SysRole selectById(Long id);
 	
+	
+	@Results(value = {
+		@Result(property = "id", column = "id", id = true),
+		@Result(property = "roleName", column="role_name"),
+		@Result(property = "enabled", column="enabled"),
+		@Result(property = "createBy", column="create_by"),
+		@Result(property = "createTime", column="create_Time"),
+	})//3.3.1后的版本@Results增加了id属性，可通过@ResultMap("@Results的id属性")，引用这个配置
+	@Select({"select id, role_name, enabled, create_by, create_time "+
+			 "from sys_role " +
+			 "where id = #{id}"})
+	SysRole selectById2(Long id);
+	
+	/*@ResultMap("XML中<resultMap>的id属性")
+	//或@ResultMap("@Results的id属性") //3.3.1后的版本
+	@Select("select * from sys_role")
+	List<SysRole> selectAll();*/
 	
 }
--- src/test/java/tk/mybatis/simple/mapper/RoleMapperTest.java
+++ src/test/java/tk/mybatis/simple/mapper/RoleMapperTest.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
 import org.junit.Test;
 
+import tk.mybatis.simple.model.SysRole;
+
 
 public class RoleMapperTest extends BaseMapperTest {
 
@@ -12,10 +14,24 @@ public class RoleMapperTest extends BaseMapperTest {
 		SqlSession sqlSession = getSqlSession();
 		try {
 			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
-			Assert.assertNotNull(roleMapper.selectById(1L));
+			SysRole sysRole = roleMapper.selectById(1L);
+			Assert.assertNotNull(sysRole);
+			Assert.assertEquals("管理员",sysRole.getRoleName());
 		} finally {
 			sqlSession.close();
 		}
+	}
 	
+	@Test
+	public void testSelectById2() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			SysRole sysRole = roleMapper.selectById2(1L);
+			Assert.assertNotNull(sysRole);
+			Assert.assertEquals("管理员",sysRole.getRoleName());
+		} finally {
+			sqlSession.close();
+		}
 	}
 }
